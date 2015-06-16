e7b5308e66c4aed72eeb725ca1b5ca5713480325
lsgo <cjy000@hotmail.com>
2015-06-16 23:06:12 +0800

    all file

---
+++ API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+
+import suds
+
+url = 'http://webservice.webxml.com.cn/WebServices/MobileCodeWS.asmx?wsdl'
+
+client = suds.client.Client(url)
+print client    #结果看图1
+print ("-------------------------------------------------")
+# result=client.service.getMobileCodeInfo(18577780801)
+result=client.service.getDatabaseInfo ()
+
+print result    #结果看图2
+print ("-------------------------------------------------")
+print client.last_received()  #结果看图3
