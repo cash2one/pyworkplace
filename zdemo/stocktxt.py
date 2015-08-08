9ee7c63f1433c18cd6052042e3fc7656e9215498
imcfy <cjy000@hotmail.com>
2015-08-09 01:53:04 +0800

    Default

--- zdemo/stocktxt.py
+++ zdemo/stocktxt.py
@@ -15,6 +15,7 @@ while line:
 myfile.close()    
 
 # print allWords[0].decode("gbk").encode("utf-8") + allWords[1].decode("gbk").encode("utf-8")
+print allWords[0] + allWords[1]
 # print allWords[2].decode("gbk").encode("utf-8")
 # print allWords[3].decode("gbk").encode("utf-8")
 # print allWords[4].decode("gbk").encode("utf-8")
