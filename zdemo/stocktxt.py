26371e8950de6f8ce2464c790dfe6bbfcfd3457f
imcfy <cjy000@hotmail.com>
2015-08-16 23:38:30 +0800

    20150816

--- zdemo/stocktxt.py
+++ zdemo/stocktxt.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
  
-myfile = open('Table.txt', 'r')  
-allWords = []  
-line = myfile.readline()  
+myFile = open('Table.txt', 'r')
+allWords = []
+line = myFile.readline()
 while line:  
-    list = line.split('    ')  
-    for word in list:  
-        if word[-1]=='\n':  
-            allWords.append(word[:-1])  #去掉行末的'\n'  
+    getList = line.split('    ')
+    for word in getList:
+        if word[-1] == '\n':
+            allWords.append(word[:-1])  # 去掉行末的'\n'
         else:  
             allWords.append(word) 
-    line = myfile.readline()  
-myfile.close()    
+    line = myFile.readline()
+myFile.close()
 
-# print allWords[0].decode("gbk").encode("utf-8") + allWords[1].decode("gbk").encode("utf-8")
-print allWords[0] + allWords[1]
+print allWords[0].decode("gbk").encode("utf-8") + allWords[1].decode("gbk").encode("utf-8")
+# print allWords[0] + allWords[1]
 # print allWords[2].decode("gbk").encode("utf-8")
 # print allWords[3].decode("gbk").encode("utf-8")
 # print allWords[4].decode("gbk").encode("utf-8")
