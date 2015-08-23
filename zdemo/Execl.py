1c85cc5e333cf7b5ef22e123d6e97f24d4eaa1a2
Lsgo <cjy000@hotmail.com>
2015-08-23 16:21:16 +0800

    20150823

--- zdemo/Execl.py
+++ zdemo/Execl.py
@@ -5,7 +5,7 @@ import MySQLdb
 import time
 
 
-def open_excel(file='Table.xls'):
+def open_excel(file='Table1.xls'):
     try:
         data = xlrd.open_workbook(file)
         return data
@@ -18,7 +18,7 @@ def iif(condition, true_part, false_part):
 
 
 # 根据索引获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_index：表的索引
-def excel_table_byindex(file='Table.xls', colnameindex=0, by_index=0):
+def excel_table_byindex(file='Table1.xls', colnameindex=0, by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows  # 行数
@@ -37,7 +37,7 @@ def excel_table_byindex(file='Table.xls', colnameindex=0, by_index=0):
 
 
 # 根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
-def excel_table_byname(file='Table.xls', colnameindex=0, by_name=u'sql'):
+def excel_table_byname(file='Table1.xls', colnameindex=0, by_name=u'sql'):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
     nrows = table.nrows  #行数
--- zdemo/stocktxt.py
+++ zdemo/stocktxt.py
@@ -5,7 +5,7 @@ import MySQLdb
 import time
 
 
-def open_excel(file='Table.xls'):
+def open_excel(file='Table1.xls'):
     try:
         data = xlrd.open_workbook(file)
         return data
@@ -18,7 +18,7 @@ def iif(condition, true_part, false_part):
 
 
 # 根据索引获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_index：表的索引
-def excel_table_byindex(file='Table.xls', colnameindex=0, by_index=0):
+def excel_table_byindex(file='Table1.xls', colnameindex=0, by_index=0):
     data = open_excel(file)
     table = data.sheets()[by_index]
     nrows = table.nrows  #行数
@@ -37,7 +37,7 @@ def excel_table_byindex(file='Table.xls', colnameindex=0, by_index=0):
 
 
 #根据名称获取Excel表格中的数据   参数:file：Excel文件路径     colnameindex：表头列名所在行的所以  ，by_name：Sheet1名称
-def excel_table_byname(file='Table.xls', colnameindex=0, by_name=u'sql'):
+def excel_table_byname(file='Table1.xls', colnameindex=0, by_name=u'sql'):
     data = open_excel(file)
     table = data.sheet_by_name(by_name)
     nrows = table.nrows  #行数
@@ -109,30 +109,30 @@ if __name__ == "__main__":
             cur.execute('delete from STKPRODUCT where STKCODE=\''+values[u'代码'][2:8]+'\'')
             cur.execute(sql, value)
             status=0
-            # # 插入交易表  values[1].decode("gbk").encode("utf-8"),
-            # if values[u'涨幅%'].find("--") == -1:
-            #   status=1
-            # else:
-            #   status=2
-
+            # 插入交易表  values[1].decode("gbk").encode("utf-8"),
+            if values[u'现价'] == '--':
+              status=1
+            else:
+              status=2
+            stockCode = values[u'代码']
             value=[values[u'代码'][2:8],   # STKCODE      varchar(12)
                     values[u'代码'][0:2],      # MARKETTYPE   varchar(2) '市场类型'
                     '20'+time.strftime("%y%m%d"),  # TRDDATE      int(8) NOT NULL COMMENT '交易日期' ,
                     values[u'昨收'],       # PRECLSPRC    varchar(255)  '昨日收盘价' ,
-                    values[u'开盘'],  # OPNPRC DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '日开盘价',
-                    values[u'最高'],       # HIPRC DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '日最高价',
-                    values[u'最低'],         # LOPRC        decimal(4,4) NULL DEFAULT NULL COMMENT '日最低价' ,
-                    values[u'现价'],    # CLSPRC       decimal(4,4) NULL DEFAULT NULL COMMENT '日收盘价' ,
-                    values[u'涨跌'],    # ADV DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '涨跌值',
-                    values[u'涨幅%'],    # ADR          decimal(4,4) NULL DEFAULT NULL COMMENT '涨跌率 * 100' ,
-                    values[u'总手'],    # VOL DECIMAL (20, 0) NULL DEFAULT NULL COMMENT '日个股交易股数 成交量',
+                    iif(status == 2,0,values[u'开盘']),  # OPNPRC DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '日开盘价',
+                    iif(status == 2,0,values[u'最高']),       # HIPRC DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '日最高价',
+                    iif(status == 2,0,values[u'最低']),         # LOPRC        decimal(4,4) NULL DEFAULT NULL COMMENT '日最低价' ,
+                    iif(status == 2,0,values[u'现价']),    # CLSPRC       decimal(4,4) NULL DEFAULT NULL COMMENT '日收盘价' ,
+                    iif(status == 2,0,values[u'涨跌']),    # ADV DECIMAL (4, 4) NULL DEFAULT NULL COMMENT '涨跌值',
+                    iif(status == 2,0,values[u'涨幅%']),    # ADR          decimal(4,4) NULL DEFAULT NULL COMMENT '涨跌率 * 100' ,
+                    iif(values[u'总手'] == '--',0,values[u'总手']),    # VOL DECIMAL (20, 0) NULL DEFAULT NULL COMMENT '日个股交易股数 成交量',
                     values[u'总金额'],    # AMOUNT       varchar(20) '日成交额' ,
                     iif(values[u'流通市值'] == '--',0,values[u'流通市值']),    # CAPITAL VARCHAR (20) '流通市值',
                     iif(values[u'总市值'] == '--',0,values[u'总市值']),    # CAPTOTAL     varchar(20) '总市值' ,
                     iif(values[u'市盈(动)'] == '--',0,values[u'市盈(动)']),    # PE DECIMAL (4, 2) NULL DEFAULT NULL COMMENT '市盈率',
                     iif(values[u'市净率'] == '--',0,values[u'市净率']),    #  PB           decimal(4,2) NULL DEFAULT NULL COMMENT '市净率' ,
-                    values[u'外盘'],    # BUYVOL       decimal(10,0) NULL DEFAULT NULL COMMENT '外盘量' ,
-                    values[u'内盘'],    # SELLVOL      decimal(10,0) NULL DEFAULT NULL COMMENT '内盘量' ,
+                    iif(values[u'外盘'] == '--',0,values[u'外盘']),    # BUYVOL       decimal(10,0) NULL DEFAULT NULL COMMENT '外盘量' ,
+                    iif(values[u'内盘'] == '--',0,values[u'内盘']),    # SELLVOL      decimal(10,0) NULL DEFAULT NULL COMMENT '内盘量' ,
                     status   # TRDSTA       varchar(2)  '交易状态
                 ]
             sql = '''INSERT INTO STKTRADE (STKCODE, MARKETTYPE, TRDDATE, PRECLSPRC,
@@ -146,6 +146,7 @@ if __name__ == "__main__":
             cur.close()
             conn.close()
 
-            print "执行成功 共" +'%d' %len(tables) +'条 现在执行第 '+ '%d' %rn +'条！'
+
         except MySQLdb.Error, e:
+            print "执行成功 共" +'%d' %len(tables) +'条 现在执行第 '+ '%d' %rn +'条！'
             print "Mysql Error %d: %s" % (e.args[0], e.args[1])
