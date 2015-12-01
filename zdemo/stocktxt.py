6fef64f0bb8974673af75491e0f435a08ff5fd85
imcfy <cjy000@hotmail.com>
2015-12-01 16:31:16 +0800

    20151201

--- zdemo/stocktxt.py
+++ zdemo/stocktxt.py
@@ -14,7 +14,8 @@ while line:
     line = myfile.readline()  
 myfile.close()    
 
-# print allWords[0].decode("gbk").encode("utf-8") + allWords[1].decode("gbk").encode("utf-8")
+print allWords[0].decode("gbk").encode("utf-8") + allWords[1].decode("gbk").encode("utf-8")
+#print allWords[0] + allWords[1]
 # print allWords[2].decode("gbk").encode("utf-8")
 # print allWords[3].decode("gbk").encode("utf-8")
 # print allWords[4].decode("gbk").encode("utf-8")
